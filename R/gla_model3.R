2b5a815a9ba763a3695b450c6930da6d9873ef8b
gaze-abyss <66149548+gaze-abyss@users.noreply.github.com>
2024-07-12 10:44:19 +0800

    Update gla_model3.R

--- R/gla_model3.R
+++ R/gla_model3.R
@@ -2,7 +2,7 @@
 #'
 #' @param pheno_column numbers
 #' @param gene_region path, txtfile
-#' @param anno path,vcffile
+#' @param anno path,txtfile
 #' @param wt path,csvfile
 #' @param phenotype path,csvfile
 #' @param filename character,vcftools output file
@@ -17,16 +17,24 @@
 #' @import dplyr
 #'
 #' @examples
-#' gla_model3("chr4A",2,"/path/data.gene.list",
-#' "/path/chr4A.annotesplit.filter.vcf",
+#' gla_model3("chr4A",
+#' 2,
+#' "/path/data.gene.list",
+#' "/path/chr4A.anno.txt",
 #' "/path/wildtype.csv",
 #' "/path/vcftoolsres/",
 #' "/path/phenotype.csv",
 #' "/respath/")
 #'
 
-gla_model3 = function(filename,pheno_column,gene_region,anno,wt,genotype_path,phenotype,outpath){
-
+gla_model3 = function(filename,
+                      pheno_column,
+                      gene_region,
+                      anno,
+                      wt,
+                      genotype_path,
+                      phenotype,
+                      outpath){
   df = fread(paste0(genotype_path,"/",filename,".012"),header = F)
   df = df[,-1]
   pos = read.table(paste0(genotype_path,"/",filename,".012.pos"))
